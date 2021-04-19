54f36526d59e3731d713d90b7c3b903ab6eb8c1c
Ripon <habibkazi92@gmail.com>
2021-04-19 21:11:09 +0800

    oder data show

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
 const cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3msfl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,24 +19,51 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
   const servicesCollection = client.db("loanServicesDB").collection("loanServices");
+  const loanCollection = client.db("loanServicesDB").collection("loanApplication");
    app.post('/addService', (req, res) => {
        const newEvent = req.body;
        servicesCollection.insertOne(newEvent)
       .then(result => {
-        console.log(result.insertedCount);
+        
         res.send(result.insertedCount > 0);
       })
 
    })
    app.get('/service', (req, res)=>{
-     servicesCollection.find()
+     servicesCollection.find({})
      .toArray((err, items)=>{
        res.send(items)
       
      })
      
    })
+
+   app.get('/service/:name', (req, res)=>{  
+     servicesCollection.find({name : req.params.name})
+    
+     .toArray((err, items)=>{
+       res.send(items);
+     })
+    
+   })
  
+  
+  app.post('/loanOrder', (req, res)=>{
+      const order = req.body;
+      loanCollection.insertOne(order)
+      .then(result=> {
+        res.send(result.insertedCount> 0);
+      })
+  })
+
+  app.get('/bookingList', (req, res)=>{
+    loanCollection.find({})
+    .toArray((err, items)=>{
+      res.send(items) 
+    })
+    
+  })
+
   console.log("collection error", err);
 });
 
