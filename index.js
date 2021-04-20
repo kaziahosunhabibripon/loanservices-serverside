e36370f466171a9961bed877ffb142f39366c945
Ripon <habibkazi92@gmail.com>
2021-04-20 13:10:18 +0800

    review done

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const servicesCollection = client.db("loanServicesDB").collection("loanServices");
   const loanCollection = client.db("loanServicesDB").collection("loanApplication");
+  const reviewCollection = client.db("loanServicesDB").collection("review");
+  
    app.post('/addService', (req, res) => {
        const newEvent = req.body;
        servicesCollection.insertOne(newEvent)
@@ -29,6 +31,22 @@ client.connect(err => {
       })
 
    })
+   app.post('/review', (req, res)=>{
+    const review = req.body;
+    reviewCollection.insertOne(review)
+    .then(result=>{
+        res.send(result.insertedCount > 0);
+    })
+    
+
+})
+  app.get('/reviewUser', (req, res)=>{
+    reviewCollection.find({})
+    .toArray((err, items)=>{
+      res.send(items)
+    })
+  })
+
    app.get('/service', (req, res)=>{
      servicesCollection.find({})
      .toArray((err, items)=>{
@@ -57,12 +75,25 @@ client.connect(err => {
   })
 
   app.get('/bookingList', (req, res)=>{
-    loanCollection.find({})
+   
+    loanCollection.find({email: req.query.email})
     .toArray((err, items)=>{
       res.send(items) 
     })
     
   })
+  // app.delete('/delete/:id', (req, res) => {
+  //   bookCollection.deleteOne({ _id: ObjectId(req.params.id) })
+  //       .then(result => {
+  //           res.send(result.deletedCount > 0);
+  //       })
+
+
+        
+  //     })
+
+      
+
 
   console.log("collection error", err);
 });
